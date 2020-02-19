2bbd2b9324cb13781e55f05037aa5f44c4cc7027
Kleber Coelho <kleber@MacBook-Pro-de-Kleber.local>
2020-02-18 21:40:28 -0300

    path test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 
 
+
+
 my_url = 'https://datastudio.google.com/reporting/188wX_8wKVwiG8VBhAGheljpcqU18Dov1/page/bCkF'
 
 
 from selenium import webdriver
+import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,6 +16,15 @@ import csv
 import schedule
 
 from datetime import datetime
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
 
 
 class medidordigital():
